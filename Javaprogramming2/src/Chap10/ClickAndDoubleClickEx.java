db3e513107e6b31ecad851b27e3c88c1623e13aa
park1997 <iambyunghyun97@gmail.com>
2021-11-15 22:32:10 +0900

    Create ClickAndDoubleClickEx.java

---
+++ Javaprogramming2/src/Chap10/ClickAndDoubleClickEx.java
@@ -0,0 +1,14 @@
+package Chap10;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ClickAndDoubleClickEx extends JFrame {
+    public ClickAndDoubleClickEx() {
+        setTitle("Click and DoubleClick 예제");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Container c = getContentPane();
+        
+    }
+}
